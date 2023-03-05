35e531dd170d41934a6023dc80eb5d1785d14d2b
Francisco Rodriguez <f.rodriguez@1xinternet.de>
2023-03-05 15:20:10 +0100

    fhinishing OOP trak

--- calculateshipping.php
+++ calculateshipping.php
@@ -1,8 +1,74 @@
 <?php
 
+class Product {
+	private $price;
+  private $weight;
+	private $freeShipping = false;
+	function __construct( $price, $weight) {
+		$this->price = $price;
+		$this->weight = $weight;
+	}
+
+	function getWeight() {
+		return $this->weight;
+	}
+	function getPrice() {
+		return $this->price;
+	}
+	function setFreeShipping() {
+		$this->freeShipping = true;
+	}
+	function getFreeShipping() {
+		return $this->freeShipping;
+	}
+}
+
+class Shipping {
+	private $totalShipping;
+	private $products;
+	private $pricePerKg;
+
+	public function __construct($priceperkg) {
+		$this->pricePerKg = $priceperkg;
+	}
+
+	public function addProducts (Product $prodcut) {
+		$this->products[] = $prodcut;
+
+	}
+
+	public function calculatetotalShipping () {
+		foreach ($this->products as $product){
+			if(!$product->getFreeShipping()){
+				$this->totalShipping +=  $product->getWeight() * $this->pricePerKg;
+			}
+		}
+	}
+
+	public function gettotalShipping () {
+		return $this->totalShipping;
+}
+}
+
+$product = new Product(5, 1);
+$product2 = new Product(7, 1);
+$product3 = new Product(17, 2);
+$product3->setFreeShipping();
+$priceperkg = 15;
+
+$shipping = new Shipping($priceperkg);
+$shipping->addProducts($product);
+$shipping->addProducts($product2);
+$shipping->addProducts($product3);
+$shipping->calculatetotalShipping();
+$total = $shipping->gettotalShipping();
+
+var_dump($total);
+
+
 
 // include functions pho
-
+/*
 function calculateShipping($productWeight, $pricePerKg, $hasfreeshipping) {
   if($hasfreeshipping){
     return 0;  
@@ -29,4 +95,4 @@ foreach ($products as $product) {
   $total = calculateShipping($product['productWeight'], $priceperkg, $product['freeshipping']);
 }
 
-echo($total);
+echo($total);*/
